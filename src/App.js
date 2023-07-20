fd6211b07ea3efe86250f8c348fa990cc829edcc
beki-d <bk14drobs@gmail.com>
2023-07-20 14:45:19 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+import Navbar2 from "./components/navbar2";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Navbar2 />
+    </div>
+  );
+}
+
+export default App;
