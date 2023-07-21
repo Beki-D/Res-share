216a55dbb6ff3e71430bf4587b94dca937392865
beki-d <bk14drobs@gmail.com>
2023-07-21 08:10:33 +0300

    notn added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,58 +5,20 @@ import Sidebar from './sidebar';
 export default function navbar() {
   return (
     <div>
-      <nav class=' bg-[#FBFBFB] dark:bg-gray-900 fixed w-full'>
+      <nav class='bg-white border-gray-200 dark:bg-gray-900'>
         <div class='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-          <div className='flex flex-row'>
-            {/* <!-- Logo --> */}
-            <a href='https://flowbite.com/' class='flex items-center'>
-              <span class='self-center text-2xl font-sans font-semibold whitespace-nowrap dark:text-white'>
-                RES<span className=' text-logo'>.</span>SHARE
-              </span>
-            </a>
-
-            {/* <!-- links --> */}
-            <div
-              class='items-center justify-between hidden w-full md:flex md:w-auto md:order-1 ml-5'
-              id='navbar-user'
-            >
-              <ul class='flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
-                <li>
-                  <a
-                    href='#'
-                    class='block py-2 pl-3 pr-4 font-sans font- font-medium text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
-                    aria-current='page'
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href='#'
-                    class='block py-2 pl-3 pr-4 font-sans text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                  >
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href='#'
-                    class='block py-2 pl-3 pr-4 font-sans text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
-                  >
-                    Services
-                  </a>
-                </li>
-              </ul>
-            </div>
+          {/* <!-- Logo --> */}
+          <a href='https://flowbite.com/' class='flex items-center'>
+            <span class='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
+              RES<span className=' text-green'>.</span>SHARE
+            </span>
+          </a>
+          {/* <!-- Responsive Search bar --> */}
+          <div className='hidden md:block'>
+            <SearchBar />
           </div>
-
-          {/* <!-- right  --> */}
+          {/* <!-- Add resource button --> */}
           <div class='flex items-center md:order-2'>
-            {/* <!-- Responsive Search bar --> */}
-            <div className='hidden md:block mr-3'>
-              <SearchBar />
-            </div>
-            {/* <!-- Add resource button --> */}
             <button
               type='button'
               class='text-black font-sans  items-center text-center bg-[#14C593] border border-1 border-[#000000]  w-[141.94px] h-[40px] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm mr-3  dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
