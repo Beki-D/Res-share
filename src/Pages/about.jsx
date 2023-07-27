f93d0ebce4af2506386f85e2e154b98706abb858
beki-d <bk14drobs@gmail.com>
2023-07-27 14:49:37 +0300

    Added a simple Card component to about page

--- src/Pages/about.jsx
+++ src/Pages/about.jsx
@@ -1,5 +1,15 @@
+import SimpleCard from '../components/SimpleCard';
+
 const About = () => {
-  return <div>About Page</div>;
+  return (
+    <div>
+      About Page
+      <SimpleCard
+        title='About page Title'
+        description='Description of the about page'
+      />
+    </div>
+  );
 };
 
 export default About;
